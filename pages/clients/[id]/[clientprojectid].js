c9ef30ee6bebaff98b37891b8d66da862b99c413
Nnamdi <nnamdi.b@gmail.com>
2022-07-07 17:53:06 +0200

    Pathing Programmatically

--- pages/clients/[id]/[clientprojectid].js
+++ pages/clients/[id]/[clientprojectid].js
@@ -7,7 +7,7 @@ export default function SelectedClientProjectPage() {
 
   return (
     <div>
-      <h1>THe Project Page for a Specific Project for a Selected Client</h1>
+      <h1>The Project Page for a Specific Project for a Selected Client</h1>
     </div>
   );
 }
--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -4,9 +4,16 @@ export default function ClientProjectPage() {
   const router = useRouter();
 
   console.log(router.query);
+
+  function loadProjectHander() {
+    // load data...
+    router.push('/clients/nnamdi/ProjectA');
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHander}>Load Project A</button>
     </div>
   );
 }
